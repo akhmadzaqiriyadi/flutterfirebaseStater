44ce9aaeee76feab5f566054652785e2b5502b73
akhmadzaqi <kidznet082@gmail.com>
2024-04-21 12:05:03 +0700

    Added login page and update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,18 +14,68 @@ class _LoginPageSate extends State<LoginPage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Column(children: [
-          Text('Hello World',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
+          child: Container(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, 
+                children: [
+                  Text(
+                    'Log in.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Image.asset(
+                    'lib/assets/images/login.png',
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Please sign in to continue.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 155), // Padding horizontal
+                            backgroundColor: Colors.purple, // Warna latar belakang oranye
+                          ),
+                        child: Text('Login'),
+                    ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-          ),
-        ]),
         ),
       ),
     );
   }
 }
-
-
