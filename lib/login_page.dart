1238fccb271db7cc71bbde5bf9d994dd59c57637
akhmadzaqi <kidznet082@gmail.com>
2024-04-22 23:33:30 +0700

    Auth logic login page and update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageSate();
 }
 
 class _LoginPageSate extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+Future signIn() async {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    },
+  );
+
+  try {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: _emailController.text, 
+      password: _passwordController.text
+    );
+    Navigator.pop(context);
+  } catch (error) {
+    Navigator.pop(context); // Menutup dialog loading
+  
+    String errorMessage = "Manjingna Gmail mbuh sandi Salah Kue!!";
+    if (error is FirebaseAuthException) {
+      if (error.code == 'user-not-found') {
+        errorMessage = "Email tidak terdaftar";
+      } else if (error.code == 'wrong-password') {
+        errorMessage = "Password salah";
+      }
+    }
+
+    // Menampilkan pesan kesalahan
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Kesalahan"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Tutup"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+
+  // void wrongEmailMessage() {
+  //   showDialog(
+  //     context: context,
+  //     builder: (context) {
+  //       return const AlertDialog(
+  //         title: Text('Salah AKUN Gamail BOLO'),
+  //       );
+  //     },
+  //   );
+  // }
+
+  // void wrongPasswordMessage() {
+  //   showDialog(
+  //     context: context,
+  //     builder: (context) {
+  //       return const AlertDialog(
+  //         title: Text('Salah Passworde BOLO'),
+  //       );
+  //     },
+  //   );
+  // }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +101,7 @@ class _LoginPageSate extends State<LoginPage> {
             child: Padding(
               padding: EdgeInsets.all(20),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, 
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     'Log in.',
@@ -41,34 +124,50 @@ class _LoginPageSate extends State<LoginPage> {
                   ),
                   SizedBox(height: 10),
                   TextFormField(
+                    controller: _emailController,
                     decoration: InputDecoration(
-                      labelText: 'Username',
+                      labelText: 'Email',
                       prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(11),
+                      ),
                     ),
                   ),
                   SizedBox(height: 10),
                   TextFormField(
+                    controller: _passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Password',
                       prefixIcon: Icon(Icons.lock),
-                      border: OutlineInputBorder(),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(11),
+                      ),
                     ),
                   ),
                   SizedBox(height: 20.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(horizontal: 155), // Padding horizontal
-                            backgroundColor: Colors.purple, // Warna latar belakang oranye
+                      GestureDetector(
+                        onTap: signIn,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 155, vertical: 18),
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius: BorderRadius.circular(11),
                           ),
-                        child: Text('Login'),
-                    ),
+                          child: Text('Login'),
+                        ),
+                      ),
                     ],
+                  ),
+                  SizedBox(height: 20),
+                  Divider(
+                    height: 1, // Tinggi garis (opsional)
+                    thickness: 1, // Ketebalan garis (opsional)
+                    color: Colors.grey, // Warna garis (opsional)
                   ),
                 ],
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,30 @@
 
 import 'package:flutter/material.dart';
 
-// import 'package:firebase_core/firebase_core.dart';
-import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './auth/auth_page.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
-//   runApp(const MyApp());
-// }
-
-void main() {
   runApp(const MyApp());
 }
 
+// void main() {
+//   runApp(const MyApp());
+// }
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthPage(),
     );
   }
 }
+
